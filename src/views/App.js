554707a86de0d4f47bb8a32053d339c2a0e5c680
esterfogaca <ester@cantarim.com>
2020-07-04 17:43:45 -0300

    feat(fetch): Implemented username request.

--- src/views/App.js
+++ src/views/App.js
@@ -5,13 +5,24 @@ import SearchBox from "../components/SearchBox";
 
 const App = () => {
   const [username, setUsername] = useState(null);
+  const [result, setResult] = useState(null);
 
   const handleSearch = (value) => {
     setUsername(value);
   };
 
   useEffect(() => {
-    if (username) console.log(username);
+    if (username) {
+      const fetchData = async () => {
+        const res = await fetch(
+          `https://api.github.com/search/users?q=${username}`
+        );
+        const data = await res.json();
+        setResult(data);
+      };
+
+      fetchData();
+    }
   }, [username]);
 
   return (
@@ -21,6 +32,16 @@ const App = () => {
           placeholder="What's on your mind"
           handleSearch={handleSearch}
         />
+        {result && (
+          <code>
+            {result.items[0] && result.items[0].repos_url && (
+              <a href={JSON.stringify(result.items[0].repos_url)}>
+                {JSON.stringify(result.items[0].repos_url)}
+              </a>
+            )}
+            {!result.items[0] && "User not found."}
+          </code>
+        )}
       </header>
     </div>
   );
